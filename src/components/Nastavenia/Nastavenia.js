c4e3f7b40414a6f42146cf80747981f554acc1bd
HopCip <Lubo.Hlavina@gmail.com>
2023-08-26 11:26:59 +0200

    Nastavenia.js layout update

--- src/components/Nastavenia/Nastavenia.js
+++ src/components/Nastavenia/Nastavenia.js
@@ -20,210 +20,207 @@ const Nastavenia = () => {
 
   return (
     <Container maxWidth="lg" sx={{ marginY: 5 }}>
-      <Box sx={{ bgcolor: '#cfe8fc' }}>
-        <Paper elevation={3}>
-          <Box>
-            <Box
-              sx={{
-                backgroundColor: 'primary.dark',
-                padding: 2,
-              }}
+      <Box>
+        <Box>
+          <Box
+            sx={{
+              backgroundColor: 'primary.dark',
+              padding: 2,
+            }}
+          >
+            <Typography
+              variant="h4"
+              gutterBottom
+              sx={{ color: 'white', margin: 'auto 0' }}
             >
-              <Typography
-                variant="h4"
-                gutterBottom
-                sx={{ color: 'white', margin: 'auto 0' }}
-              >
-                Nastavenia
-              </Typography>
-            </Box>
-            <Box sx={{ flexGrow: 1 }}>
-              <Grid container spacing={0}>
-                <Grid item xs={6}>
-                  <Item sx={{ paddingLeft: 3 }}>
-                    <Typography variant="h6" align="left" gutterBottom>
-                      Všeobecné
-                    </Typography>
-                    <FormGroup>
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Dostávať upozornenia na novinky"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Tmavý režim"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Povoliť výzvy"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Oznámenia o udalostiach"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Moji priatelia ma môžu vyzvať v hocičom"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Môj blog môžu vidieť"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Povoliť pozvánky do klubov"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Povoliť pozvánky do turnajov"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Bezpečnostný režim
-"
-                      />
-                    </FormGroup>
-                  </Item>
-                </Grid>
-                <Grid item xs={6}>
-                  <Item sx={{ paddingLeft: 3 }}>
-                    <Typography variant="h6" align="left" gutterBottom>
-                      Šachovnica
-                    </Typography>
-                    <FormGroup>
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Kreslenie šípok"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Robenie predťahov"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Vždy ukazovať notáciu"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Vždy spúšťať motor"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Zobrazovať čas"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Zobrazovať správne ťahy"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Zobrazovať komentáre"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Miesto na poznámky"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Vždy meraj čas"
-                      />
-                    </FormGroup>
-                  </Item>
-                </Grid>
-                <Grid item xs={6}>
-                  <Item sx={{ paddingLeft: 3 }}>
-                    <Typography variant="h6" align="left" gutterBottom>
-                      Pokročilé
-                    </Typography>
-                    <FormGroup>
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Zdieľané účty"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Automatické platby"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Po platbe zmazať kartu"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Nezobrazovať históriu diagramov"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Nezobrazovať históriu lekcií"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Nezobrazovať históriu kvízov"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Anonymný člen"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Mód inkognito"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Povoliť cookies vždy"
-                      />
-                    </FormGroup>
-                  </Item>
-                </Grid>
-                <Grid item xs={6}>
-                  <Item sx={{ paddingLeft: 3 }}>
-                    <Typography variant="h6" align="left" gutterBottom>
-                      Profil
-                    </Typography>
-                    <FormGroup>
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Ukazovať stav"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Ukazovať krajinu"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="všetci môžu vidieť moj profil"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Všetci mi môžu posielať správy"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Všetci môžu vidieť môj progres"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Všetci môžu vidieť mojich priateľov"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Všetci ma môžu vyzvať"
-                      />
-                      <FormControlLabel
-                        control={<Switch defaultChecked />}
-                        label="Všetci ma môžu vyhľadať"
-                      />
-                      <FormControlLabel
-                        control={<Switch />}
-                        label="Všetci môžu vidieť moju profilovku"
-                      />
-                    </FormGroup>
-                  </Item>
-                </Grid>
-              </Grid>
-            </Box>
+              Nastavenia
+            </Typography>
           </Box>
-        </Paper>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <Item sx={{ paddingLeft: 3 }}>
+                  <Typography variant="h6" align="left" gutterBottom>
+                    Všeobecné
+                  </Typography>
+                  <FormGroup>
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Dostávať upozornenia na novinky"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Tmavý režim"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Povoliť výzvy"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Oznámenia o udalostiach"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Moji priatelia ma môžu vyzvať v hocičom"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Môj blog môžu vidieť"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Povoliť pozvánky do klubov"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Povoliť pozvánky do turnajov"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Bezpečnostný režim"
+                    />
+                  </FormGroup>
+                </Item>
+              </Grid>
+              <Grid item xs={6}>
+                <Item sx={{ paddingLeft: 3 }}>
+                  <Typography variant="h6" align="left" gutterBottom>
+                    Šachovnica
+                  </Typography>
+                  <FormGroup>
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Kreslenie šípok"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Robenie predťahov"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Vždy ukazovať notáciu"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Vždy spúšťať motor"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Zobrazovať čas"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Zobrazovať správne ťahy"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Zobrazovať komentáre"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Miesto na poznámky"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Vždy meraj čas"
+                    />
+                  </FormGroup>
+                </Item>
+              </Grid>
+              <Grid item xs={6}>
+                <Item sx={{ paddingLeft: 3 }}>
+                  <Typography variant="h6" align="left" gutterBottom>
+                    Pokročilé
+                  </Typography>
+                  <FormGroup>
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Zdieľané účty"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Automatické platby"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Po platbe zmazať kartu"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Nezobrazovať históriu diagramov"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Nezobrazovať históriu lekcií"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Nezobrazovať históriu kvízov"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Anonymný člen"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Mód inkognito"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Povoliť cookies vždy"
+                    />
+                  </FormGroup>
+                </Item>
+              </Grid>
+              <Grid item xs={6}>
+                <Item sx={{ paddingLeft: 3 }}>
+                  <Typography variant="h6" align="left" gutterBottom>
+                    Profil
+                  </Typography>
+                  <FormGroup>
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Ukazovať stav"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Ukazovať krajinu"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="všetci môžu vidieť moj profil"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Všetci mi môžu posielať správy"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Všetci môžu vidieť môj progres"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Všetci môžu vidieť mojich priateľov"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Všetci ma môžu vyzvať"
+                    />
+                    <FormControlLabel
+                      control={<Switch defaultChecked />}
+                      label="Všetci ma môžu vyhľadať"
+                    />
+                    <FormControlLabel
+                      control={<Switch />}
+                      label="Všetci môžu vidieť moju profilovku"
+                    />
+                  </FormGroup>
+                </Item>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
       </Box>
     </Container>
   )
